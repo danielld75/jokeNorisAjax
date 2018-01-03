19ca4b209352c40459f2e4c008c2a53379a33de5
danielld75 <danielld@poczta.fm>
2018-01-03 14:51:18 +0100

    Invoke getJoke function as parameter in click fun

--- js/main.js
+++ js/main.js
@@ -2,22 +2,21 @@ $(function () {
     var url = 'http://api.icndb.com/jokes/random';
     var $paragraph = $('#joke');
 
-    $('#get-joke').click(function () {
-        getJoke();
-    });
-
-    getJoke();
-
     function getJoke() {
         $.ajax({
             url: url,
             method: 'GET',
             success: function (res) {
-                $paragraph.text(res.value.joke);
+                $paragraph.html(res.value.joke);
             },
             error: function () {
                 $paragraph.text("Happen something wrong");
             }
         })
     }
+
+    getJoke();
+
+    var $button = $('#get-joke').click(getJoke);
+
 });
